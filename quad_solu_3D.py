c321e079e45da3a5098a542c136cd337c4db0aed
idoneve <39467668+idoneve@users.noreply.github.com>
2022-06-05 15:38:42 -0600

    Update and rename 3d.py to quad_solu_3D.py

--- quad_solu_3D.py
+++ quad_solu_3D.py
@@ -9,20 +9,15 @@ def quadratic(a, b, c):
 
 ax = plt.axes(projection='3d')
 
-# z = np.linspace(0, 100, 1000)
-# x = np.sqrt(z)
-# y = 2 ** z
-# plt.axes(projection='3d').plot3D(x, y, z)
-
 def f(x, y):
-    return quadratic(x, 0, y)
+    return quadratic(x, 0, y) # change variable locations to change what x and y graph
 
 x = np.linspace(-10, 10, 100)
 y = np.linspace(-10, 10, 100)
 X, Y = np.meshgrid(x, y)
 
 Z = f(X, Y)[0]
-ax.plot_surface(X, Y, Z, rstride=10, cstride=10, cmap='viridis', edgecolor='none')
+ax.plot_surface(X, Y, Z, rstride=10, cstride=10, cmap='viridis', edgecolor='none') # change these lines to change how the graph looks
 Z = f(X, Y)[1]
 ax.plot_surface(X, Y, Z, rstride=10, cstride=10, cmap='viridis', edgecolor='none')
 
